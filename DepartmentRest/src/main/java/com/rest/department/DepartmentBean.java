e7f5c50fe7dfe36f1d021bc6eb46898a6a712395
khalsa <saurabh.itbhu04@gmail.com>
2016-08-17 02:26:09 -0400

    Initial Commit

---
+++ DepartmentRest/src/main/java/com/rest/department/DepartmentBean.java
@@ -0,0 +1,29 @@
+package com.rest.department;
+
+public class DepartmentBean {
+	
+	public int departmentId;
+	public String departmentName;
+	
+	public DepartmentBean(int dId, String dName){
+		this.departmentId = dId;
+		this.departmentName = dName;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public String getDepartmentName() {
+		return departmentName;
+	}
+
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	
+}
