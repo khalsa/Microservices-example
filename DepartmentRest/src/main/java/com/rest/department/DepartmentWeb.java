9a95e539c3f3dffebe89ffe644beda6aff01265c
khalsa <saurabh.itbhu04@gmail.com>
2016-08-17 02:27:15 -0400

    Initial Commit

---
+++ DepartmentRest/src/main/java/com/rest/department/DepartmentWeb.java
@@ -0,0 +1,13 @@
+package com.rest.department;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class DepartmentWeb extends SpringBootServletInitializer{
+	public static void main(String args[]){
+		SpringApplication.run(DepartmentWeb.class, args);
+	}
+}
